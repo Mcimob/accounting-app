f2451179c0f1dd603d70e1ca889e9b454b403afa
Mcimob <burckhardttim@gmail.com>
2025-02-26 10:09:29 +0100

    Made BaseRepository extend JpaSpecificationExecutor

--- src/main/java/ch/pfaditools/accounting/backend/repository/BaseRepository.java
+++ src/main/java/ch/pfaditools/accounting/backend/repository/BaseRepository.java
@@ -6,10 +6,11 @@ import ch.pfaditools.accounting.model.loadtype.HasLoadType;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import java.util.Optional;
 
-public interface BaseRepository<T extends AbstractEntity, F extends AbstractFilter<T>> extends JpaRepository<T, Long> {
+public interface BaseRepository<T extends AbstractEntity, F extends AbstractFilter<T>> extends JpaRepository<T, Long>, JpaSpecificationExecutor<T> {
 
     public Optional<T> findOne(F filter, HasLoadType loadType);
 
