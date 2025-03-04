2a6eb5bcd1162f64924b4aabca66a787e0905e87
Mcimob <burckhardttim@gmail.com>
2025-03-04 11:30:20 +0100

    Removed redundant views

--- src/main/java/ch/pfaditools/accounting/ui/views/security/LogoutView.java
+++ src/main/java/ch/pfaditools/accounting/ui/views/security/LogoutView.java
@@ -3,12 +3,12 @@ package ch.pfaditools.accounting.ui.views.security;
 import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
-import jakarta.annotation.security.RolesAllowed;
+import jakarta.annotation.security.PermitAll;
 
 import static ch.pfaditools.accounting.ui.ViewConstants.ROUTE_LOGOUT;
 
 @Route(ROUTE_LOGOUT)
-@RolesAllowed({"USER", "ADMIN"})
+@PermitAll
 public class LogoutView extends VerticalLayout {
     public LogoutView() {
         UI.getCurrent().getPage().setLocation("/logout");
