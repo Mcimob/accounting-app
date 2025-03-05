ace5846b68b391bcfb4a6eeaab176d3364842ddd
Mcimob <burckhardttim@gmail.com>
2025-03-05 09:56:02 +0100

    Added link to LoginView to get to RegisterView

--- src/main/java/ch/pfaditools/accounting/ui/views/security/LoginView.java
+++ src/main/java/ch/pfaditools/accounting/ui/views/security/LoginView.java
@@ -1,6 +1,8 @@
 package ch.pfaditools.accounting.ui.views.security;
 
 import com.vaadin.flow.component.UI;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.login.LoginForm;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
@@ -10,6 +12,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 
 import static ch.pfaditools.accounting.ui.ViewConstants.ROUTE_LOGIN;
+import static ch.pfaditools.accounting.ui.ViewConstants.ROUTE_REGISTER;
 
 @Route(ROUTE_LOGIN)
 @AnonymousAllowed
@@ -23,8 +26,14 @@ public class LoginView extends VerticalLayout {
 
         LoginForm loginForm = new LoginForm();
         loginForm.setAction("login");
-
+        loginForm.setForgotPasswordButtonVisible(false);
         add(loginForm);
+
+        Button registerButton = new Button("Don't have an account yet? Register here");
+        registerButton.addThemeVariants(ButtonVariant.LUMO_TERTIARY_INLINE);
+        registerButton.addClickListener(e -> UI.getCurrent().navigate(ROUTE_REGISTER));
+        add(registerButton);
+
         setAlignItems(Alignment.CENTER);
         setJustifyContentMode(JustifyContentMode.CENTER);
         setSizeFull();
