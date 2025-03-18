b13f0dc86dbf3fc40415b3e052e39691852e7fb1
Mcimob <burckhardttim@gmail.com>
2025-03-18 17:22:28 +0100

    Changed SecurityUtils

--- src/main/java/ch/pfaditools/accounting/security/SecurityUtils.java
+++ src/main/java/ch/pfaditools/accounting/security/SecurityUtils.java
@@ -1,19 +1,30 @@
 package ch.pfaditools.accounting.security;
 
+import ch.pfaditools.accounting.model.entity.GroupEntity;
+import ch.pfaditools.accounting.model.entity.UserEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Optional;
 
 public final class SecurityUtils {
 
     private SecurityUtils() {
     }
 
-    public static String getAuthenticatedUsername() {
+    public static UserEntity getCurrentUser() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        if (authentication != null && authentication.getPrincipal() instanceof UserDetails) {
-            return ((UserDetails) authentication.getPrincipal()).getUsername();
+        if (authentication != null && authentication.getPrincipal() instanceof UserEntity) {
+            return (UserEntity) authentication.getPrincipal();
         }
         return null; // User is not logged in
+    }
+
+    public static String getAuthenticatedUsername() {
+        return Optional.ofNullable(getCurrentUser()).map(UserEntity::getUsername).orElse(null);
+    }
+
+    public static GroupEntity getAuthenticatedUserGroup() {
+        return Optional.ofNullable(getCurrentUser()).map(UserEntity::getGroup).orElse(null);
     }
 }
