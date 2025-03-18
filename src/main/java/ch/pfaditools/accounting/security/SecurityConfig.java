b23b9af7eb3b9a7034378fef49ec521bfd3c6349
Mcimob <burckhardttim@gmail.com>
2025-03-18 17:46:01 +0100

    Changed login-success landing to ReceiptOverView

--- src/main/java/ch/pfaditools/accounting/security/SecurityConfig.java
+++ src/main/java/ch/pfaditools/accounting/security/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import static ch.pfaditools.accounting.ui.ViewConstants.ROUTE_LOGIN;
+import static ch.pfaditools.accounting.ui.ViewConstants.ROUTE_RECEIPT_OVERVIEW;
 
 @Configuration
 @EnableWebSecurity
@@ -19,7 +20,9 @@ public class SecurityConfig extends VaadinWebSecurity {
         http.authorizeHttpRequests(auth ->
                 auth.requestMatchers(new AntPathRequestMatcher("/public/**")).permitAll());
 
-        setLoginView(http, ROUTE_LOGIN);
+        http.formLogin(form -> form
+                .loginPage("/" + ROUTE_LOGIN)
+                .defaultSuccessUrl("/" + ROUTE_RECEIPT_OVERVIEW));
         super.configure(http);
     }
 }
