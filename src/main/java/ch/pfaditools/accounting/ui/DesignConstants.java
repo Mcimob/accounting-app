f0361335a443ff6a9b1b880e2a7f20a74bf23d83
Mcimob <burckhardttim@gmail.com>
2025-03-18 16:17:07 +0100

    Added some styling

---
+++ src/main/java/ch/pfaditools/accounting/ui/DesignConstants.java
@@ -0,0 +1,17 @@
+package ch.pfaditools.accounting.ui;
+
+public final class DesignConstants {
+
+    private DesignConstants() { }
+
+    public static final String VIEW = "view";
+    public static final String VIEW_WIDE = "view-wide";
+
+    public static final String STYLE_FW_700 = "fw-700";
+
+    public static final String CLR_ACCENT = "var(--clr-accent)";
+    public static final String CLR_BG = "var(--clr-bg)";
+    public static final String CLR_LIGHT = "var(--clr-light)";
+    public static final String CLR_REGULAR = "var(--clr-regular)";
+    public static final String CLR_DARK = "var(--clr-dark)";
+}
