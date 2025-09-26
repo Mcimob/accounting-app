6cad569b85728c9d74f4d55808b0870b2ef645e2
Mcimob <burckhardttim@gmail.com>
2025-09-26 19:26:26 +0200

    Created PaymentCard

--- src/main/java/ch/pfaditools/accounting/ui/DesignConstants.java
+++ src/main/java/ch/pfaditools/accounting/ui/DesignConstants.java
@@ -9,6 +9,8 @@ public final class DesignConstants {
 
     public static final String STYLE_CONTENT_MATCH_WIDTH = "content-match-width";
     public static final String STYLE_WIDTH_NARROW = "width-narrow";
+
+    public static final String STYLE_FW_500 = "fw-500";
     public static final String STYLE_FW_700 = "fw-700";
 
     public static final String STYLE_WIDTH_FULL = "width-full";
@@ -18,10 +20,15 @@ public final class DesignConstants {
     public static final String STYLE_FLEX_CENTER = "flex-center";
     public static final String STYLE_FLEX_BETWEEN = "flex-between";
     public static final String STYLE_FLEX_ALIGN_START = "flex-align-start";
+    public static final String STYLE_FLEX_JUSTIFY_END = "flex-justify-end";
 
     public static final String STYLE_PADDING_S = "padding-s";
     public static final String STYLE_PADDING_M = "padding-m";
     public static final String STYLE_PADDING_L = "padding-l";
+
+    public static final String STYLE_MARGIN_S = "margin-s";
+    public static final String STYLE_MARGIN_M = "margin-m";
+    public static final String STYLE_MARGIN_L = "margin-l";
 
     public static final String STYLE_GAP_S = "gap-s";
     public static final String STYLE_GAP_M = "gap-m";
@@ -41,6 +48,8 @@ public final class DesignConstants {
     public static final String STYLE_BG_DARK = "bg-dark";
     public static final String STYLE_BG_LIGHT_ACCENT = "bg-light-accent";
     public static final String STYLE_BG_TRANSITION = "bg-transition";
+
+    public static final String STYLE_TEXT_COLOR_WHITE = "text-color-white";
 
     public static final String CLR_ACCENT = "var(--clr-accent)";
     public static final String CLR_BG = "var(--clr-bg)";
--- src/main/java/ch/pfaditools/accounting/ui/views/entity/payment/PaymentOverView.java
+++ src/main/java/ch/pfaditools/accounting/ui/views/entity/payment/PaymentOverView.java
@@ -4,14 +4,20 @@ import ch.pfaditools.accounting.backend.service.PaymentService;
 import ch.pfaditools.accounting.model.entity.PaymentEntity;
 import ch.pfaditools.accounting.model.entity.ReceiptEntity;
 import ch.pfaditools.accounting.model.filter.PaymentEntityFilter;
+import ch.pfaditools.accounting.security.SecurityUtils;
 import ch.pfaditools.accounting.ui.MainLayout;
+import ch.pfaditools.accounting.ui.components.CardDetailGrid;
 import ch.pfaditools.accounting.ui.provider.PaymentProvider;
 import ch.pfaditools.accounting.ui.views.entity.AbstractEntityOverView;
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.details.Details;
+import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.grid.GridVariant;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.router.Route;
 import jakarta.annotation.security.RolesAllowed;
+
+import java.util.List;
 
 import static ch.pfaditools.accounting.security.SecurityConstants.ROLE_ADMIN;
 import static ch.pfaditools.accounting.security.SecurityConstants.ROLE_GROUP_ADMIN;
@@ -28,26 +34,41 @@ public class PaymentOverView extends AbstractEntityOverView<PaymentEntity, Payme
 
     @Override
     protected Component createGrid() {
-        grid.addColumn(PaymentEntity::getTitle)
+        Grid.Column<PaymentEntity> cardColumn = grid.addComponentColumn(PaymentCard::new);
+        grid.addThemeVariants(GridVariant.LUMO_NO_BORDER, GridVariant.LUMO_NO_ROW_BORDERS);
+
+        if (SecurityUtils.isUserInAnyRole(ROLE_ADMIN)) {
+            return new CardDetailGrid<>(grid, createAdminColumns(), cardColumn);
+        }
+
+        return grid;
+    }
+
+    private List<Grid.Column<PaymentEntity>> createAdminColumns() {
+        Grid.Column<PaymentEntity> titleColumn = grid.addColumn(PaymentEntity::getTitle)
                 .setHeader(getTranslation("entity.payment.title"))
                 .setSortable(true)
                 .setSortProperty("title");
-        grid.addColumn(PaymentEntity::getReceiptsAmount)
+        Grid.Column<PaymentEntity> receiptsAmountColumn = grid.addColumn(PaymentEntity::getReceiptsAmount)
                 .setHeader(getTranslation("entity.receipt.amount"))
                 .setSortable(true)
                 .setSortProperty("receiptsAmount");
-        grid.addComponentColumn(p -> {
+        Grid.Column<PaymentEntity> receiptsColumn = grid.addComponentColumn(p -> {
             Details details = new Details("%s %s".formatted(
                     p.getReceipts().size(), getTranslation("entity.payment.receipts")));
-                    p.getReceipts().stream().map(ReceiptEntity::getName).map(Div::new).forEach(details::add);
-                    return details;
+            p.getReceipts().stream().map(ReceiptEntity::getName).map(Div::new).forEach(details::add);
+            return details;
         }).setHeader(getTranslation("entity.payment.receipts"));
-        grid.addColumn(pay -> pay.getCreatedDateTimeString(getLocale()))
+        Grid.Column<PaymentEntity> createdColumn = grid.addColumn(pay -> pay.getCreatedDateTimeString(getLocale()))
                 .setHeader(getTranslation("entity.abstract.createdDateTime"))
                 .setSortable(true)
                 .setSortProperty("createdDateTime");
 
-        return grid;
+        return List.of(
+                titleColumn,
+                receiptsAmountColumn,
+                receiptsColumn,
+                createdColumn);
     }
 
     @Override
